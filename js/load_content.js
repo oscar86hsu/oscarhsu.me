2c977349e9a605b76c079dc9049823321602175e
oscar86hsu <oscar86hsu@gmail.com>
2020-05-07 18:43:13 +0800

    Modify content loading script

--- js/load_content.js
+++ js/load_content.js
@@ -1,4 +1,52 @@
-$.getJSON("lang/en.json", function (content) {
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+var lang = getUrlParameter('lang');
+
+if(lang == "cht")
+{
+    var file_name = "lang/cht.json";
+    $("#lang_link").html("English");
+    $("#lang_link").attr("href", "index.html?lang=en");
+    $("#resume_file").attr("href", "files/HungHaoHsuResumeCHT.pdf");
+    $(".about-text").html("關 於 我");
+    $(".experience-text").html("經 歷");
+    $(".skills-text").html("技 能");
+    $(".certifications-text").html("證 照");
+    $(".education-text").html("學 歷");
+    $(".interests-text").html("興 趣");
+    $(".specialty-text").html("專長");
+    $(".lang-and-tools-text").html("程式語言及工具");
+}
+else
+{
+    var file_name = "lang/en.json";
+    $("#lang_link").html("中文");
+    $("#lang_link").attr("href", "index.html?lang=cht");
+    $("#resume_file").attr("href", "files/HungHaoHsuResumeEN.pdf");
+    $(".about-text").html("About");
+    $(".experience-text").html("Experience");
+    $(".skills-text").html("Skills");
+    $(".certifications-text").html("Certifications");
+    $(".education-text").html("Education");
+    $(".interests-text").html("Interests");
+    $(".specialty-text").html("Specialty");
+    $(".lang-and-tools-text").html("Programming Languages &amp; Tools");
+}
+
+$.getJSON(file_name, function (content) {
     $("#name").html(content.first_name + ' <span class="text-primary">' + content.last_name + '</span>')
     $("#profile_text").text(content.profile_text);
     
@@ -19,6 +67,10 @@ $.getJSON("lang/en.json", function (content) {
 
     $.each(content.certifications, function (index, value) {
         $("#certifications").append('<li><i class="fa-li fas fa-certificate text-warning"></i>' + value + '</li>');
+    })
+
+    $.each(content.specialty, function (index, value) {
+        $("#specialty").append('<li><i class="fa-li fa fa-check"></i>' + value + '</li>');
     })
 
     $.each(content.education, function (index, value) {
